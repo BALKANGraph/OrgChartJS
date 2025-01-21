0e142b839a82f76d41c50ec358f8d1fe6b837ef1
Zornitsa Serbezova <zornitsa.pesheva@outlook.com>
2025-01-21 11:48:01 +0200

    auto

--- orgchart.d.ts
+++ orgchart.d.ts
@@ -13,6 +13,12 @@ declare class OrgChart extends OrgChartBase {
 
 
     /**
+     *  ```typescript     
+     * var chart = new OrgChart('#tree', {});
+     * chart.load(nodes);
+     * chart.update({ id: 1, name: "Updated Name", title: "Updated Title" });
+     * chart.draw();
+     * ```
      * Updates the node data
      * @param newData node data
      */

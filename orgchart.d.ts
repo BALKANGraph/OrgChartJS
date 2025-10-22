968aa94d84d50d955b8e6195cf942c0f25afdb09
balkangraph <zornitsa.pesheva@outlook.com>
2025-10-22 07:12:01 +0300

    auto

--- orgchart.d.ts
+++ orgchart.d.ts
@@ -3986,7 +3986,7 @@ declare namespace OrgChart {
     }
 
         /**
-         * Menu options
+         * Controls are small buttons displayed on the chart that let users quickly perform actions such as exporting, zooming, switching layouts, or fitting the chart to the screen.
          * ```typescript     
          * var chart = new OrgChart('#tree', {
          *      controls:{
@@ -4001,6 +4001,7 @@ declare namespace OrgChart {
          * {@link https://balkan.app/OrgChartJS/Docs/Controls | See doc...}
          */
     interface controls  {
+        
         [key: string]: {
             title: string,
             icon?: string,
